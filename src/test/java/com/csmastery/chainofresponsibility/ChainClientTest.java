d30308934517e1a1a0d786296154e8a22115f37b
minimalgeek <minimalgeek@gmail.com>
2020-06-25 12:28:48 +0200

    reorganize unit test for chain

--- src/test/java/com/csmastery/chainofresponsibility/ChainClientTest.java
+++ src/test/java/com/csmastery/chainofresponsibility/ChainClientTest.java
@@ -17,17 +17,17 @@ public class ChainClientTest {
 
   @Test
   public void nameShouldBeNotEmpyty() {
-    assertFalse(chainClient.validateUser(User.builder().age(50).email("hello@bello.com").build()));
+    assertFalse(chainClient.validateUser(User.builder().name("").age(50).email("hello@bello.com").build()));
   }
 
   @Test
   public void ageShouldBeBiggerThan18() {
-    assertFalse(chainClient.validateUser(User.builder().name("Peti").email("hello@bello.com").build()));
+    assertFalse(chainClient.validateUser(User.builder().name("Peti").age(17).email("hello@bello.com").build()));
   }
 
   @Test
   public void emailShouldBeValid() {
-    assertFalse(chainClient.validateUser(User.builder().age(22).name("Peti").email("valami").build()));
+    assertFalse(chainClient.validateUser(User.builder().name("Peti").age(22).email("valami").build()));
   }
 
   @Test
