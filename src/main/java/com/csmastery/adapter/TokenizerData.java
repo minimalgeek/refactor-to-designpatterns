c9620b9796a942c272622b7cab1edc83cb3c94af
minimalgeek <minimalgeek@gmail.com>
2020-07-21 18:08:45 +0200

    :poop: add incomplete tests for Adapter pattern

---
+++ src/main/java/com/csmastery/adapter/TokenizerData.java
@@ -0,0 +1,11 @@
+package com.csmastery.adapter;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TokenizerData {
+  private String data;
+  private String separator;
+}
