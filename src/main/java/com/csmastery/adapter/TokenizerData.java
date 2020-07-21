a63b7ebd04304981b87e88ae0c3f41b866158f91
minimalgeek <minimalgeek@gmail.com>
2020-07-21 21:27:13 +0200

    replace @Data with @Getters, since I only need those

--- src/main/java/com/csmastery/adapter/TokenizerData.java
+++ src/main/java/com/csmastery/adapter/TokenizerData.java
@@ -1,9 +1,9 @@
 package com.csmastery.adapter;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class TokenizerData {
   private String data;
