b863220feec62e749076436db1197e8ed2422a55
Tamas Floch <tamasfloch95@gmail.com>
2020-07-27 10:34:03 +0200

    UserBuilderDesignPattern solution

--- src/main/java/com/csmastery/builder/User.java
+++ src/main/java/com/csmastery/builder/User.java
@@ -9,37 +9,7 @@ public class User {
   private String name;
   private double height;
 
-  public User() throws Exception {
-    this(null);
-  }
-
-  public User(String email) throws Exception {
-    if (Strings.isNullOrEmpty(email))
-      throw new Exception("Email shouldn't be empty");
-    this.email = email;
-  }
-
-  public User(String email, int age) throws Exception {
-    this(email);
-    this.age = age;
-  }
-
-  public User(String email, String name) throws Exception {
-    this(email);
-    this.name = name;
-  }
-
-  public User(String email, double height) throws Exception {
-    this(email);
-    this.height = height;
-  }
-
-  public User(String email, String name, int age, double height) throws Exception {
-    this(email);
-    this.name = name;
-    this.age = age;
-    this.height = height;
-  }
+  private User() { }
 
   public String getEmail() {
     return email;
@@ -57,4 +27,46 @@ public class User {
     return height;
   }
 
+  public static UserBuilder builder() {
+    return new UserBuilder();
+  }
+
+  public static class UserBuilder {
+    private User user = new User();
+
+    private UserBuilder() { }
+
+    public UserBuilder setEmail(String email){
+      user.email = email;
+      return this;
+    }
+
+    public UserBuilder setAge(int age){
+      user.age = age;
+      return this;
+    }
+
+    public UserBuilder setName(String name){
+      user.name = name;
+      return this;
+    }
+
+    public UserBuilder setHeight(double height){
+      user.height = height;
+      return this;
+    }
+
+    public UserBuilder reset() {
+      user = new User();
+      return this;
+    }
+
+    public User build() throws Exception {
+      if(Strings.isNullOrEmpty(user.email))
+        throw new Exception("Email shouldn't be empty");
+      if(user.age < 18)
+        throw new Exception("Too young");
+      return user;
+    }
+  }
 }
