d20cdb2abe1bf9f1dd6239274fd924476d893bbf
Boti <kissiki99@gmail.com>
2020-07-30 10:18:36 +0200

    statePatternSolution Done

--- src/main/java/com/csmastery/state/Goku.java
+++ src/main/java/com/csmastery/state/Goku.java
@@ -1,48 +1,27 @@
 package com.csmastery.state;
 
-import java.util.stream.IntStream;
-
 public class Goku {
 
-  private TransformationLevel currentLevel = TransformationLevel.FIGHTER;
+  private State currentState = new FighterState();
 
   public TransformationLevel powerUp() {
-    TransformationLevel[] values = TransformationLevel.values();
-    int levelIndex = IntStream.range(0, values.length).filter(i -> currentLevel.equals(values[i])).findFirst()
-        .orElse(-1);
-    currentLevel = values[(levelIndex + 1) % values.length];
-    return currentLevel;
+    this.currentState = currentState.nextState();
+    return currentState.getLevel();
   }
 
   public TransformationLevel getCurrentLevel() {
-    return currentLevel;
+    return currentState.getLevel();
   }
 
   public String hairColor() {
-    return switch (this.currentLevel) {
-      case FIGHTER -> "Black";
-      case SSJ1 -> "Yellow";
-      case SSJ2 -> "Even More Yellow";
-      case SSJ3 -> "Total Yellow and Long";
-    };
+      return currentState.hairColor();
   }
 
   public int attackValue() {
-    return switch (this.currentLevel) {
-      case FIGHTER -> 9000;
-      case SSJ1 -> 100_000;
-      case SSJ2 -> 100_000_000;
-      case SSJ3 -> 500_000_000;
-    };
+    return currentState.attackValue();
   }
 
   public boolean tryToDestroyFrieza(Frieza frieza) {
-    if (this.currentLevel == TransformationLevel.FIGHTER) {
-      return false;
-    } else {
-      frieza.makeHimDead();
-      return true;
-    }
+      return currentState.tryToDestroyFrieza(frieza);
   }
-
 }
